b01b1ebb798a14578e32b00c0be2a7521e8872b3
John Duavis <paulduavis09@gmail.com>
2025-05-04 23:38:48 +0800

    The null-coalescing operator

---
+++ NullOperator/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+void OldSchoolLogSring(string theString) //the old school way of logging
+{
+    if (theString == null)
+    {
+        Console.WriteLine("No string provided");
+    }
+    else
+    {
+        Console.WriteLine(theString);
+    }
+}
+
+void LogString(string theString) // the new school way of logging
+{
+    Console.WriteLine(theString ?? "No string provided");
+}
+
+void ThrowableLogString(string theString) // the new school way of logging
+{
+    Console.WriteLine(theString ?? throw new ArgumentNullException(nameof(theString), "No string provided"));
+}
+
+
+OldSchoolLogSring("Old School Log String");
+LogString("New School Log String");
+ThrowableLogString("Throwable Log String");
+
+string str = "Hello, World!";
+str ??= "Default Value"; // Using null-coalescing assignment operator
+Console.WriteLine(str); // Output: Default Value
