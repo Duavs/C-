967d8d5fc4fc39fdbee86d2cf5877efca3e4bcd5
John Duavis <paulduavis09@gmail.com>
2025-05-04 22:27:21 +0800

    using underscore as a number separator

---
+++ NumberLiterals/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+// you can use _ as a number separator for better readability
+
+int number = 1_344_500;
+float f = 4_353.5f;
+var x = 0xAB_CD_EF;
+var b = 0b1101_1110_1001_0010;
+
+Console.WriteLine($"number: {number}");
+Console.WriteLine($"float: {f}");
+Console.WriteLine($"hex: {b:X}");
+Console.WriteLine($"binary: {x:X}");
