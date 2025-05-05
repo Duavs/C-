6d2a3e2b72c2b75a124338a0f661d87a91948b63
John Duavis <paulduavis09@gmail.com>
2025-05-05 23:50:55 +0800

    Added DeconstructCustom

---
+++ DeconstructCustom/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DefaultNamespace;
+//Create some classes with data
+Dog Belgian = new Dog("Charlie", 4, "Belgian", "Black and Brown");
+Dog Aspin = new Dog("Max", 5, "Aspin", "Pure white");
+//Use Deconstruction to get the name and colors
+var (name, colors) = Belgian;
+Console.WriteLine($"Name: {name}, Colors: {colors}");
+
+//Use Deconstruction to get the name, age, and breed
+(name, var age, var breed) = Aspin;
+Console.WriteLine($"Name: {name}, Age: {age}, Breed: {breed}");
