49fdb59246e695f0a859726acccbb59bcc8d9abf
John Duavis <paulduavis09@gmail.com>
2025-05-08 23:42:19 +0800

    changes

---
+++ SwitchExpression/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+// can now operate on just about any type of object
+using DefaultNamespace;
+string ShapeToString(object shape)
+{
+    switch (shape)
+    {
+        case Circle:
+            return "Circle";
+        case Rectangle:
+            return "Rectangle";
+        case Triangle:
+            return "Triangle";
+        default:
+            return "Unknown";
+    }
+}
+
+Console.WriteLine(ShapeToString(new Circle{Radius = 10}));
