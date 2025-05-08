0aaa80a98bae3fb2a6fa91f7b7248f0254b9077f
John Duavis <paulduavis09@gmail.com>
2025-05-08 23:42:03 +0800

    SwitchExpression

---
+++ SwitchExpression/Shapes.cs
@@ -0,0 +1,21 @@
+namespace DefaultNamespace;
+
+public class Shapes
+{
+    public class Rectangle
+    {
+        public int Length { get; set; }
+        public int Width { get; set; }
+    }
+
+    public class Circle
+    {
+        public int Radius { get; set; }
+    }
+    
+    public class Triangle
+    {
+        public int Base { get; set; }
+        public int Height { get; set; }
+    }
+}
