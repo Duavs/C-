1758f9bef1f5830b8e301188efbcfe40fb83aa58
John Duavis <paulduavis09@gmail.com>
2025-05-14 23:18:17 +0800

    changes

---
+++ Classes/Indexers/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+StockRecords stock1 = new StockRecords();
+
+Console.WriteLine($"Average: {stock1.Average:C}");
+Console.WriteLine($"High: {stock1.High:C}");
+Console.WriteLine($"Low: {stock1.Low:C}");
+
+//use the indexer
+Console.WriteLine($"Days: {stock1.Length}");
+
+for (int i = 0; i < stock1.Length); i++)
+{
+    decimal =  stock1[i];
+    Console.WriteLine($"Val: {val:C}}");
+} 
+Console.WriteLine((""))
