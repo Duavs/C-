ffe855de18c4557f0d89b6417777d54231537815
John Duavis <paulduavis09@gmail.com>
2025-05-15 23:23:15 +0800

    implement value equality for Point2D by overriding Equals, GetHashCode, and comparison operators

---
+++ Classes/Comparing/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+Point2D p1 = new Point2D() { X = 10, Y = 20 };
+Point2D p2 = new Point2D() { X = 10, Y = 20 };
+
+//Test for equality
+Console.WriteLine($"{p1 == p2}"); // This will not work as expected
+Console.WriteLine($"{p1 != p2}");
+Console.WriteLine($"{p1.Equals(p2)}"); // This will work as expected
+
+
+public class Point2D
+{
+    public int X;
+    public int Y;
+    
+    //override the methods needed to define value equality
+    public override bool Equals(object? obj) => Equals(obj as Point2D);
+    public bool Equals(Point2D? p) => p != null && (X == p.X && Y == p.Y);
+    public override int GetHashCode() => (X, Y).GetHashCode();
+    
+    //Override the == and != operators
+    public static bool operator ==(Point2D? p1, Point2D? p2)
+    {
+        if (p1 is null)
+        {
+            if (p2 is null)
+            {
+                return true;
+            }
+            return false;
+        }
+        return p1.Equals(p2);
+    }
+
+    public static bool operator !=(Point2D? p1, Point2D? p2) => !(p1 == p2);
+}
---
+++ ToString/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+float x = 42.0f;
+string str = x.ToString();
+Console.WriteLine($"{str}");
+
+//ToString to represent a custom type
+Person p = new Person() {FirstName = "John", LastName = "Duavis", Age=25};
+Console.WriteLine($"{p}");
+public class Person
+{
+    public string? FirstName
+    {
+        get; set;
+    }
+
+    public string? LastName
+    {
+        get;
+        set;
+    }
+
+    public int Age
+    {
+        get;
+        set;
+    }
+    public override string ToString()
+    {
+        return $"[Person] FirstName:{FirstName}, LastName:{LastName}, Age:{Age}";
+    }
+}
+
+//override toString 
+//public override string ToString() => $"[Person] FirstName:{FirstName}, LastName:{LastName}, Age:{Age}";
