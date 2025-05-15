4acdb99f55ba87380f39c159dabec20fe2b894de
John Duavis <paulduavis09@gmail.com>
2025-05-15 20:42:55 +0800

    feat: add string indexer support to StockRecords for day-based lookup

--- Classes/Indexers/Program.cs
+++ Classes/Indexers/Program.cs
@@ -1,11 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
-
+using DefaultNamespace;
 StockRecords stock1 = new StockRecords();
 
 Console.WriteLine($"Average: {stock1.Average:C}");
 Console.WriteLine($"High: {stock1.High:C}");
-// Console.WriteLine($"Low: {stock1.ToString();
+Console.WriteLine($"Low: {stock1.Low:C}");
 
 //use the indexer
 Console.WriteLine($"Days: {stock1.Length}");
@@ -16,3 +16,8 @@ for (int i = 0; i < stock1.Length; i++)
     Console.WriteLine($"Val: {val:C}");
 } 
 Console.WriteLine();
+
+Console.WriteLine($"Monday: {stock1["mon"]:C}");
+Console.WriteLine($"Wednesday: {stock1["wed"]:C}");
+
+Console.WriteLine($"Not Valid Day: {stock1["monwed"]: C}");
